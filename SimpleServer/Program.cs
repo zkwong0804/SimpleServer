42890c906796489226f09e38ed5a73dcb869c2f3
DK\zhe <zhenkai.wong@sitecore.com>
2020-11-21 19:28:14 +0800

    Add project files.

---
+++ SimpleServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleServer
+{
+    class Program
+    {
+        private const int PORT = 1314;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We are getting the server ready...");
+            Socket sc = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            NetworkStream ns = null;
+            sc.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            Console.WriteLine($"The server are listening for any request from port {PORT}");
+            sc.Listen(3);
+            try
+            {
+                ns = new NetworkStream(sc.Accept());
+                ns.CopyTo(Console.OpenStandardOutput());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\n\nSomething went wrong...");
+                Console.WriteLine($"Exception type: {ex.GetType()}");
+                Console.WriteLine($"\n\n\t{ex.StackTrace}");
+            }
+            finally
+            {
+                if (sc != null) sc.Close();
+                if (ns != null) ns.Close();
+            }
+
+            Console.WriteLine("Bye bye!");
+            
+        }
+    }
+}
